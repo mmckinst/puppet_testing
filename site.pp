402f872f74c39b0acfb4b729dad1c43b131ffa27
Mark McKinstry <mmckinst@nexcess.net>
2015-05-07 12:07:47 -0400

    work around for puppet bugs

--- site.pp
+++ site.pp
@@ -1,1 +1,4 @@
-hiera_include('classes')
+# https://tickets.puppetlabs.com/browse/PUP-4334
+# https://tickets.puppetlabs.com/browse/PUP-4046
+# hiera_include('classes')
+include(hiera_array(classes))
